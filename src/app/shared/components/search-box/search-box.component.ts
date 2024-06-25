9e3abb048455c1bf24dd5eec68b4e95796742f53
Leo Ss <leo.vm.cba@gmail.com>
2024-06-25 16:01:33 -0300

    add: debouncer function to search

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,15 +1,31 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+  private debouncer: Subject<string> = new Subject<string>();
+
   @Input() public placeholder: string = '';
   @Output() public onValue = new EventEmitter<string>();
+  @Output() public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.debouncer.pipe(debounceTime(500)).subscribe((value) => {
+      this.onDebounce.emit(value);
+    });
+  }
 
   emitValue(value: string): void {
     this.onValue.emit(value);
+  }
+
+  onKeyPress(value: string) {
+    // validar que el value contiene algo
+    if (!value) return;
+    this.debouncer.next(value);
   }
 }
