2474665aeb53fa22094c9db49f7736ba261e9b4e
Leo Ss <leo.vm.cba@gmail.com>
2024-06-25 18:15:04 -0300

    add: data persistence on reload

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -17,7 +17,19 @@ export class CountriesService {
     byRegion: { region: '', countries: [] },
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+  }
+
+  private loadFromLocalStorage() {
+    if (!localStorage.getItem('cacheStore')) return;
+
+    this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+  }
 
   private getCountriesRequest(url: string): Observable<Country[]> {
     return this.http.get<Country[]>(url).pipe(
@@ -40,7 +52,8 @@ export class CountriesService {
       tap(
         (countries) =>
           (this.cacheStore.byCapital = { term: capital, countries: countries })
-      )
+      ),
+      tap(() => this.saveToLocalStorage())
     );
   }
 
@@ -51,7 +64,8 @@ export class CountriesService {
       tap(
         (countries) =>
           (this.cacheStore.byCountry = { term: country, countries: countries })
-      )
+      ),
+      tap(() => this.saveToLocalStorage())
     );
   }
 
@@ -62,7 +76,8 @@ export class CountriesService {
       tap(
         (countries) =>
           (this.cacheStore.byRegion = { region: region, countries: countries })
-      )
+      ),
+      tap(() => this.saveToLocalStorage())
     );
   }
 }
