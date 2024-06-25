b46348983824a4c1a552f5f3bcdfe0e01afb03f4
Leo Ss <leo.vm.cba@gmail.com>
2024-06-25 16:49:19 -0300

    modify: by region UI

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,13 +12,22 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
     this.isLoading = true;
 
-    this.countriesService.searchRegion(term).subscribe((countries) => {
+    this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
       this.isLoading = false;
     });
