baa45a22ccb109f132d708efc18b343ff16cb5ac
Leo Ss <leo.vm.cba@gmail.com>
2024-06-26 16:55:43 -0300

    fix: show/hide sidebar function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,11 @@ export class AppComponent implements OnInit {
   isDesktop: boolean = window.innerWidth >= 768;
 
   ngOnInit(): void {
+    // Monitorear cambios de pantalla (responsive) para mostrar/ocultar el sidebar
     this.onResize();
+
+    // Cuando se genera el componente si esta en una pantalla desktop seteamos isSidebarVisible en true
+    if (this.isDesktop) this.isSidebarVisible = true;
   }
 
   toggleSidebar() {
